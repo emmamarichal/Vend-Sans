5cab4ce43dd3c2d374d747124fbcdf3ca0746d7d
Kristian Möller <kristian@ktkm.se>
2025-03-03 11:00:24 +0100

    1.610
    /ampersand /oslash fix

--- sources/build.sh
+++ sources/build.sh
@@ -3,6 +3,6 @@ set -e
 
 echo "Build Vend Sans and Vend Sans Italic"
 
-gftools builder /Users/Kristian/Documents/GitHub/Dicotype/Vend-Sans/sources/config.yaml
+gftools builder /Vend-Sans/sources/config.yaml
 
 echo "Nice and Done!"
